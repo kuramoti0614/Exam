4cc6aa2c2fa341ccc07eb3386575ddc6ffcd54ff
倉持秀豊 <@LAPTOP-5BDB0UMI>
2026-05-07 10:39:44 +0900

    新たな追加

---
+++ src/main/java/DAO/TestListStudent.java
@@ -0,0 +1,11 @@
+package DAO;
+
+
+
+public class TestListStudent extends Dao {
+	
+	private String baseSql = "select * from student where school_cd = ?";
+	
+	
+	public postFilter(rSet,ResultSet)
+}
--- src/main/java/scoremanager/main/TestListSubjectExecuteAction.java
+++ src/main/java/scoremanager/main/TestListSubjectExecuteAction.java
@@ -1,1 +1,29 @@
 package scoremanager.main;
+
+import java.util.List;
+
+import DAO.SubjectDao;
+import bean.School;
+import bean.Subject;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class TestListSubjectExecuteAction {
+
+    public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+
+        // セッションから学校情報を取得
+        School school = (School) req.getSession().getAttribute("school");
+
+        // DAO 呼び出し
+        SubjectDao dao = new SubjectDao();
+        List<Subject> list = dao.filter(school);
+
+        // JSP に渡す
+        req.setAttribute("subject_list", list);
+
+        // 表示画面へ
+        req.getRequestDispatcher("/subject_list.jsp")
+           .forward(req, res);
+    }
+}
